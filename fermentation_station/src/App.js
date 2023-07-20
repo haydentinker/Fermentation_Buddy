8c35223b3ad3c5873b7f5a613cf24b958d7c9f13
haydentinker <haydentinker613@gmail.com>
2023-07-20 15:54:47 -0700

    Starting to integrate with firebase for database and userauth

--- fermentation_station/src/App.js
+++ fermentation_station/src/App.js
@@ -1,25 +1,33 @@
-import {React} from "react";
+import { React } from "react";
 import { ProjectWrapper } from "./pages/ProjectWrapper";
 import './css/App.css'
 import { NavBar } from "./components/NavBar";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Calendar } from "./pages/Calendar";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import firebaseConfig from './config';
 function App() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  getDb(db);
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className="container">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/projects" element={<ProjectWrapper/>}/>
-        <Route path="/calendar" element={<Calendar/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectWrapper />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </div>
-      </>
-    );
+    </>
+  );
 }
-
+async function getDb(db) {
+  console.log(db)
+}
 export default App;
